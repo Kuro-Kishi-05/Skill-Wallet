6c82195514be4c6c561266b237b7661a82ffed6b
Kuro-Kishi-05 <soquilajonathan@gmail.com>
2026-05-15 23:48:41 +0800

    Add files via upload

---
+++ debug-project/script.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+
+let name = "John";
+console.log(name.toUpperCase());
+
+let num = 10;
+if (num > 5) {
+    console.log("Big number");
+}
+
+    let x = 5;
+console.log("Value of x:", x);
+
+let num = 3;
+if (num > 5) {
+    console.log("Big");
+} else {
+    console.log("Small");
+}
